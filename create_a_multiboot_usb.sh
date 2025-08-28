ca12924153c7a551407b9f1937069665d87756b4
v <>
2025-08-28 22:01:42 +0200

    Cleaner code, safer, copy ISOs

--- create_a_multiboot_usb.sh
+++ create_a_multiboot_usb.sh
@@ -1,26 +1,30 @@
 #!/bin/sh
 
-# Security checks and infos
-if [[ $EUID -ne 0 ]]; then
-   echo "This script must be run as root" 
-   exit 1
-fi
-read -p "Enter USB name (i.e: sda) : " usb
-echo "WARNING: This will delete ALL data on /dev/$usb!"
-read -p "Are you sure you want to continue? (y/N): " confirm
-if [[ $confirm != "y" && $confirm != "Y" ]]; then
-    echo "Operation cancelled."
-    exit 1
-fi
+# Infos and checks
+[ "$(id -u)" -ne 0 ] && echo "This script must be run as root" && exit 1
+read -p "Enter USB name (i.e: sda): " usb
+echo "WARNING: This will delete ALL data on /dev/${usb}!"
+read -p "Are you sure? (y/N): " confirm
+[ "$confirm" != "y" ] && [ "$confirm" != "Y" ] && echo "Cancelled." && exit 1
 
-sfdisk --delete /dev/$usb
-echo 'type=EFI' | sfdisk /dev/$usb
-mkfs.vfat -F 32 /dev/$usb
+# Format USB
+wipefs -a /dev/$usb
+echo -e 'label: gpt\n, , U' | sfdisk /dev/$usb
+mkfs.vfat -F 32 /dev/${usb}1
+
+# Mount and setup
+mkdir -p /mnt/usb
 mount /dev/${usb}1 /mnt/usb
-mkdir /mnt/usb/{boot,boot-isos,other-isos}
+mkdir -p /mnt/usb/{boot,boot-isos,other-isos}
 
-grub-install --target=x86_64-efi --removable --boot-directory=/mnt/usb/boot --efi-directory=/mnt
-curl -o https://raw.githubusercontent.com/Devaler/MBUSB-script/main/grub.cfg /mnt/usb/boot/grub/grub.cfg
+# Install GRUB
+grub-install --target=x86_64-efi --removable --boot-directory=/mnt/usb/boot --efi-directory=/mnt/usb
+curl https://raw.githubusercontent.com/Devaler/MBUSB-script/main/grub.cfg -o /mnt/usb/boot/grub/grub.cfg
+
+# Copy ISOs
+read -p "Path to ISO files (enter for current dir): " isodir
+isodir=${isodir:-.}
+[ -d "$isodir" ] && rsync -ah --progress --no-compress --whole-file --inplace --size-only --no-owner --no-group "$isodir"/*.iso /mnt/usb/boot-isos/
 
 umount /mnt/usb
 rm -rf /mnt/usb
